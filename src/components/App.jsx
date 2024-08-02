47ad790fb511c99d0c15649bf08caead5958e4a9
Paul Simfukwe <91340984+Paul-Simfukwe@users.noreply.github.com>
2024-08-02 15:40:45 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input name="fName" placeholder="First Name" onChange={handleChange} />
+        <input name="lName" placeholder="Last Name" onChange={handleChange} />
+        <input name="email" placeholder="Email" onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
